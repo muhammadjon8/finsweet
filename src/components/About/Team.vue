6031135bdaffbc8571a95688898ff5098b619471
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-06 01:57:19 +0500

    #

--- src/components/About/Team.vue
+++ src/components/About/Team.vue
@@ -34,14 +34,37 @@ export default {
 
 <template>
   <div class="py-16 bg-[#F4F6FC]">
-    <div class="container">
+    <div class="container mx-auto">
       <h3 class="text-center text-[28px] font-semibold py-5">Meet Our Team</h3>
-      <div class="grid grid-cols-4 py-5 gap-5">
-        <div v-for="(person, index) in arr" :key="index">
-          <div class="bg-white flex flex-col items-center w-[290px] py-5">
-            <img :src="person.image" alt="" class="py-5"/>
-            <h3 class="text-[24px] font-medium">{{ person.name }}</h3>
-            <p class="text-black/50">{{ person.job }}</p>
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-5 gap-5 group"
+      >
+        <div
+          v-for="(person, index) in arr"
+          :key="index"
+          class="flex justify-center"
+        >
+          <div
+            class="bg-white flex flex-col items-center w-[290px] py-5 relative overflow-hidden group"
+          >
+            <div
+              class="absolute top-[40px] left-[58px] w-[168px] h-[168px] bg-[url('/blur.png')] bg-no-repeat bg-center z-20 hidden group-hover:top[40px] group-hover:block duration-200"
+            >
+              <div class="flex gap-3 absolute top-[120px] left-[50px]">
+                <a href="facebook.com" class="z-30"
+                  ><img src="../../assets/facebook.svg" alt=""
+                /></a>
+                <a href="instagam.com" class="z-30"
+                  ><img src="../../assets/instagram.svg" alt=""
+                /></a>
+                <a href="twitter.com" class="z-30"
+                  ><img src="../../assets/twitter.svg" alt=""
+                /></a>
+              </div>
+            </div>
+            <img :src="person.image" alt="" class="py-5 relative" />
+            <h3 class="text-[24px] font-medium relative">{{ person.name }}</h3>
+            <p class="text-black/50 relative">{{ person.job }}</p>
           </div>
         </div>
       </div>
