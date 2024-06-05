b8211d95794c034b0f2c9ea2f105752b3b00c142
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-05 10:11:31 +0500

    #

--- src/components/About/Goals.vue
+++ src/components/About/Goals.vue
@@ -1,25 +1,26 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="py-9">
     <div class="container">
       <div class="flex flex-col">
         <div class="flex">
-          <h2>Goals to focus</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-            reiciendis veritatis magni quos omnis. Possimus error aliquam
-            quibusdam placeat dicta in, voluptas explicabo veniam sint porro
-            doloremque beatae aperiam, sit enim omnis ad, totam ratione
-            recusandae hic quae adipisci. Sunt quisquam ducimus ratione
-            excepturi assumenda aliquid delectus quia dicta possimus eos,
-            repudiandae in necessitatibus odit ex. Magni voluptas asperiores
-            temporibus assumenda, dolor quos quo veniam eligendi, distinctio
-            architecto officiis obcaecati sit harum fugiat odio eveniet
-            molestias debitis autem voluptate fugit quam aut, tempore explicabo
-            minima! Eos cumque accusantium assumenda provident iure sit, maiores
-            esse aperiam, fugiat repellat suscipit veritatis rem?
-          </p>
+          <div>
+            <h2>Goals focused</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+              tenetur aliquid esse corrupti recusandae quaerat ipsum nemo
+              placeat excepturi aliquam.
+            </p>
+          </div>
+          <div>
+            <h2>Continuous Development</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+              tenetur aliquid esse corrupti recusandae quaerat ipsum nemo
+              placeat excepturi aliquam.
+            </p>
+          </div>
         </div>
       </div>
     </div>
