938ea9e86466c0f30bb5aeee9f196eddda8dd04e
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-05 11:01:14 +0500

    #

--- src/components/About/Goals.vue
+++ src/components/About/Goals.vue
@@ -3,24 +3,31 @@
 <template>
   <div class="py-9">
     <div class="container">
-      <div class="flex flex-col">
-        <div class="flex">
+      <div class="flex flex-col bg-[#F4F5F5]">
+        <div class="flex px-9 py-9">
           <div>
-            <h2>Goals focused</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
-              tenetur aliquid esse corrupti recusandae quaerat ipsum nemo
-              placeat excepturi aliquam.
+            <h2 class="font-semibold text-[28px] py-5">Goals focused</h2>
+            <p class="w-4/5 text-[14px] text-black/50">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Veritatis, officia velit et nemo minima incidunt iure. Cum maxime,
+              soluta necessitatibus laborum assumenda mollitia, sint, totam qui
+              optio repudiandae ab animi!
             </p>
           </div>
           <div>
-            <h2>Continuous Development</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
-              tenetur aliquid esse corrupti recusandae quaerat ipsum nemo
-              placeat excepturi aliquam.
+            <h2 class="font-semibold text-[28px] py-5">
+              Continuous Development
+            </h2>
+            <p class="w-4/5 text-[14px] text-black/50">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              distinctio. Veniam eius maxime accusantium id vero odio, aut,
+              omnis a adipisci ipsa aperiam? Qui, saepe a. Officia debitis
+              consectetur porro?
             </p>
           </div>
+        </div>
+        <div>
+          <img src="../../assets/man.png" alt="" />
         </div>
       </div>
     </div>
---
+++ src/components/About/Team.vue
@@ -0,0 +1,19 @@
+<script>
+import { ref } from 'vue';
+
+const arr = ref([
+    {
+        name: 'John',
+        job: 'Developer',
+        image: ""
+    }
+]);
+</script>
+
+<template>
+  <div>
+    <h2 class="text-[28px] font-semibold">Meet Our Team</h2>
+  </div>
+</template>
+
+<style scoped></style>
