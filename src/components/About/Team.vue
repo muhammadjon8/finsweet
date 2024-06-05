c442359cdf45b497e4259f8e4e5a670c052f8b47
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-06 01:03:57 +0500

    #

--- src/components/About/Team.vue
+++ src/components/About/Team.vue
@@ -1,18 +1,51 @@
 <script>
-import { ref } from 'vue';
+import { reactive } from "vue";
 
-const arr = ref([
-    {
-        name: 'John',
-        job: 'Developer',
-        image: ""
-    }
+const arr = reactive([
+  {
+    name: "John Smith",
+    job: "Developer",
+    image: "/team1.png",
+  },
+  {
+    name: "Simon Adams",
+    job: "CTO",
+    image: "/team4.png",
+  },
+  {
+    name: "Paul Jones",
+    job: "Design Lead",
+    image: "/team3.png",
+  },
+  {
+    name: "Sara Hardin",
+    job: "Project Manager",
+    image: "/team2.png",
+  },
 ]);
+export default {
+  setup() {
+    return {
+      arr,
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <h2 class="text-[28px] font-semibold">Meet Our Team</h2>
+  <div class="py-16 bg-[#F4F6FC]">
+    <div class="container">
+      <h3 class="text-center text-[28px] font-semibold py-5">Meet Our Team</h3>
+      <div class="grid grid-cols-4 py-5 gap-5">
+        <div v-for="(person, index) in arr" :key="index">
+          <div class="bg-white flex flex-col items-center w-[290px] py-5">
+            <img :src="person.image" alt="" class="py-5"/>
+            <h3 class="text-[24px] font-medium">{{ person.name }}</h3>
+            <p class="text-black/50">{{ person.job }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,12 +1,14 @@
 <script setup>
 import ABoutUs from "../components/About/AboutUs.vue";
 import Goals from "../components/About/Goals.vue";
+import Team from "../components/About/Team.vue";
 </script>
 
 <template>
   <div>
     <ABoutUs />
     <Goals />
+    <Team />
   </div>
 </template>
 
