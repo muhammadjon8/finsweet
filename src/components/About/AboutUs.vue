8053da0998766270c08935771b0456017c378fc4
muhammadjon8 <umuhammadjon22@gmail.com>
2024-06-07 11:07:05 +0500

    #

--- src/components/About/AboutUs.vue
+++ src/components/About/AboutUs.vue
@@ -2,21 +2,21 @@
 <template>
   <div>
     <div class="container">
-      <div class="flex py-9 items-center">
-        <div class="w-1/2">
-          <div class="w-2/3 flex flex-col gap-5">
-            <h3 class="text-[18px] font-normal">About us</h3>
-            <h2 class="text-[44px] font-semibold leading-tight">
+      <div class="md:flex py-9 items-center max-sm:w-full">
+        <div class="md:w-1/2">
+          <div class="md:w-2/3 flex flex-col gap-5">
+            <h3 class="text-[18px] font-normal max-sm:text-center">About us</h3>
+            <h2 class="text-[44px] font-semibold leading-tight max-sm:text-center">
               Our designs solve problems
             </h2>
-            <p class="text-black/50 text-[12px]">
+            <p class="text-black/50 text-[12px] max-sm:text-center">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
               voluptatum mollitia ab tempora ex pariatur dolorem beatae nulla,
               in itaque?
             </p>
           </div>
         </div>
-        <div class="w-1/2">
+        <div class="md:w-1/2">
           <img src="../../assets/people.png" alt="" />
         </div>
       </div>
--- src/components/About/Goals.vue
+++ src/components/About/Goals.vue
@@ -4,10 +4,10 @@
   <div class="py-9">
     <div class="container">
       <div class="flex flex-col bg-[#F4F5F5]">
-        <div class="flex px-9 py-9">
+        <div class="md:flex px-9 py-9">
           <div>
             <h2 class="font-semibold text-[28px] py-5">Goals focused</h2>
-            <p class="w-4/5 text-[14px] text-black/50">
+            <p class="md:w-4/5 text-[14px] text-black/50">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Veritatis, officia velit et nemo minima incidunt iure. Cum maxime,
               soluta necessitatibus laborum assumenda mollitia, sint, totam qui
@@ -18,7 +18,7 @@
             <h2 class="font-semibold text-[28px] py-5">
               Continuous Development
             </h2>
-            <p class="w-4/5 text-[14px] text-black/50">
+            <p class="md:w-4/5 text-[14px] text-black/50">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
               distinctio. Veniam eius maxime accusantium id vero odio, aut,
               omnis a adipisci ipsa aperiam? Qui, saepe a. Officia debitis
@@ -27,7 +27,7 @@
           </div>
         </div>
         <div>
-          <img src="../../assets/man.png" alt="" />
+          <img src="../../assets/man.png" alt="" class="max-sm:h-full"/>
         </div>
       </div>
     </div>
